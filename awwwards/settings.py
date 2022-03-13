1416e0b7a6270f5dc679bf40327d40b31e3d447c
trevin-livele <liveletrevin6@gmail.com>
2022-03-13 12:59:44 +0300

    added static

--- awwwards/settings.py
+++ awwwards/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'awwwards.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    'awwwards/static',
+]
+
