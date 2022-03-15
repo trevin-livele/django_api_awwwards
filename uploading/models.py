ac2935b4b1ad1fb01c016dd5fa380d74ef466848
trevin-livele <liveletrevin6@gmail.com>
2022-03-15 17:22:42 +0300

    created review rating model

--- uploading/models.py
+++ uploading/models.py
@@ -17,3 +17,19 @@ class Post(models.Model):
 
     def __str__(self):
         return self.description
+
+
+class ReviewRating(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100,blank=True)
+    review = models.TextField(max_length=500,blank=True)
+    rating = models.FloatField()
+    ip = models.CharField(max_length=20, blank=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.subject
