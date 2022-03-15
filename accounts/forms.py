b3a681327342079da23301fdb9d3fd78454bf1c1
trevin-livele <liveletrevin6@gmail.com>
2022-03-15 12:34:38 +0300

    added registration

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,12 @@
 from django import forms
-from .models import Account,Userprofile
+from .models import Account
 
 
 class RegistrationForm (forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Enter Password'
     }))
 
     confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'confirm Password'
     }))
     class Meta:
         model = Account
@@ -17,10 +15,6 @@ class RegistrationForm (forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
-        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
-        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter last Name'
-        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
-        self.fields['email'].widget.attrs['placeholder'] = 'Enter email'
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,73 @@
 from django.shortcuts import redirect, render,get_object_or_404
+from accounts.models import Account
+from .forms import RegistrationForm,UserForm
+from.models import Account
 from django.contrib import messages,auth
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login,logout,authenticate
 
 
+
 # Create your views here.
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+            user = Account.objects.create_user(first_name=first_name,last_name=last_name,
+                                                email=email,username=username,password=password)
+            user.phone_number = phone_number
+            user.is_active = True
+            user.save()
+
+
+
+
+            # login(request, user)
+            #USER ACTIVATION
+            # send_email.send()
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'accounts/register.html' ,context)
 
 
 def login_user(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = authenticate(request,email=email,password=password)
+
+        if user is not None:
+            login(request,user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
+@login_required(login_url = 'login')
 def logout(request):
+    logout(request)
+    messages.success(request, 'You are logged out.')
     return redirect('login')
+
+
+
+
+
+
+
+
+
