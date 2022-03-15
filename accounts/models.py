ff31974015e79a83697a33204e69d8e9b0aff3bb
trevin-livele <liveletrevin6@gmail.com>
2022-03-15 10:02:19 +0300

    created model post

--- accounts/models.py
+++ accounts/models.py
@@ -85,3 +85,17 @@ class Account(AbstractBaseUser):
 
     def has_module_perms(self, add_label):
         return True
+
+
+
+
+class Post(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField()
+    description = models.CharField(max_length=200)
+    date_posted  =  models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.description
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render,get_object_or_404
 from django.contrib import messages,auth
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login,logout,authenticate
+from .models import Post
 
 
 # Create your views here.
@@ -15,3 +16,14 @@ def login_user(request):
 
 def logout(request):
     return redirect('login')
+
+
+
+def createpost(request):
+    if request.method == 'POST':
+        image = request.FILES.get('image')
+        description = request.POST.get('description')
+        post = Post.objects.create(user=request.user,image=image,description=description)
+        post.save()
+        return redirect('home')
+    return render(request, 'create_post.html')
